5ce755a53f8cad128de3fdb230a7de5c516c416a
Cesar Pulles <cesarpulles@gmail.com>
2020-10-15 10:07:38 +0200

    Added force accept for desktop install

--- puppet/manifests/vm-corrections.pp
+++ puppet/manifests/vm-corrections.pp
@@ -17,7 +17,7 @@ node 'dev-box' {
 	}
 
 	exec { 'Install ubuntu-desktop':
-		command => '/usr/bin/apt-get install ubuntu-desktop',
+		command => '/usr/bin/apt-get -o DPkg::Options::=--force-confdef install -y ubuntu-desktop',
 		require => Exec['Update and Upgrade ubuntu']
 	}
 
