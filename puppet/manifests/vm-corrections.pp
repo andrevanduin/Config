b5c255a8381e06918717472c34badde9a92e22a3
Cesar Pulles <cesarpulles@gmail.com>
2020-10-22 11:31:35 +0200

    Removed commented out lines and changed puppet to focal

--- puppet/manifests/vm-corrections.pp
+++ puppet/manifests/vm-corrections.pp
@@ -1,31 +1,7 @@
 node 'dev-box' {
-
-
-	# exec { 'Add git repository for mate':
-	# 	command => '/usr/bin/apt-add-repository ppa:ubuntu-mate-dev/xenial-mate',
-	# 	onlyif => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-mate-dev-ubuntu-ppa-xenial.list'
-	# }
-
-	# exec { 'Remove original xenial mate file': 
-	# 	command => '/bin/rm -rf /etc/apt/sources.list.d/ubuntu-mate-dev-ubuntu-ppa-xenial.list*',
-	# 	onlyif => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-mate-dev-ubuntu-ppa-xenial.list',
-	# 	require => Exec['Add git repository for mate']
-	# }
-
-	# exec { 'Update and Upgrade ubuntu':
-	# 	command => '/usr/bin/apt-get -o DPkg::Options::=--force-confdef update -y && /usr/bin/apt-get -o DPkg::Options::=--force-confdef upgrade -y'
-	# }
-
 	exec { 'Install ubuntu-desktop':
 		command => '/usr/bin/apt-get -o DPkg::Options::=--force-confdef install -y ubuntu-desktop',
-		require => Exec['Update and Upgrade ubuntu']
 	}
-
-  #       file_line { 'Keyboard Mapping':
-  #               path  => '/etc/default/keyboard',
-  #               line  => 'XKBLAYOUT="us"',
-  #               match => '^XKBLAYOUT="ch"',
-	# }
 }
 
 
